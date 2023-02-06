7e22494802436e778c20ddbf23a56b7fd281813d
wombatDaiquiri <wombatDaiquiri@proton.me>
2023-02-06 23:15:35 +0100

    add static landing

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ FROM gcr.io/distroless/base-debian10
 
 WORKDIR /
 COPY --from=build /wombat-src/wombat /wombat
+COPY --from=build /wombat-src/static /static
 
 EXPOSE 8080
 USER nonroot:nonroot
--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
+
+func serveStatic(filename string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filename)
+	}
+}
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome, but updated"))
-	})
-	http.ListenAndServe(":8080", r)
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(time.Second * 60))
+
+	r.Get("/", serveStatic("static/index.html"))
+	r.Get("/style.css", serveStatic("static/style.css"))
+	r.Get("/favicon.ico", serveStatic("static/favicon.ico"))
+	r.Get("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))).ServeHTTP)
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	fmt.Println("stopping the server")
 }
